23604dd204e1cf149c2554ea773eff92e669c2ff
david <weiqiang.zhao@msxf.com>
2021-08-15 17:26:13 +0800

    first commit

---
+++ TestSDKDemo/TestSDK.framework/Headers/TestA.h
@@ -0,0 +1,18 @@
+//
+//  TestA.h
+//  TestSDK
+//
+//  Created by zhaoweiqiang on 2021/8/13.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestA : NSObject
+
++ (void)log;
+
+@end
+
+NS_ASSUME_NONNULL_END
